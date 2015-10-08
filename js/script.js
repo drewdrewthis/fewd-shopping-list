3c2abc7cbb8a011e5bcdd67ca56ffb8164abd2b6
Andrew Joia <drewdrewthis@gmail.com>
2015-10-08 00:40:37 -0400

    Moves all var inits to top of js file and out of individual functions

--- js/script.js
+++ js/script.js
@@ -2,50 +2,41 @@ $(document).ready(function() {
 
 // Create starter list array
 
-var items = [
-	{
-		"name" : "Item 1",
-		"class" : ""
-	},
-	{
-		"name" : "Item 2",
-		"class" : "editing"
-	},
-	{
-		"name" : "Finished!",
-		"class" : "finished"
-	},
-	{
-		"name" : "Item 3",
-		"class" : "done"
-	}
-];
+var newItem = $('input').val(),
+	newItemString = "",
+	htmlString = "",
+	C = "",
+	I = "",
+	N = "",
+	items = [
+		{
+			"name" : "Finished!",
+			"class" : "finished"
+		}
+	];
 
 // Class = C, Index = I, Name = N
 // Uncaught SyntaxError: Block-scoped declarations (let, const, function, class) not yet supported outside strict mode
 
 function createItemString( C, I, N) {
 	
-	var htmlString = '<li class="item ' + C + '" value="' + I + '"><span class="item-btn"></span><i class="fa fa-exclamation"></i><span class="item-name">' + N + '</span><i class="fa fa-angle-down"></i><div class="controls"><button class="important">IMPORTANT!</button><button class="delete">DELETE</button></div></li>';
+	htmlString = '<li class="item ' + C + '" value="' + I + '"><span class="item-btn"></span><i class="fa fa-exclamation"></i><span class="item-name">' + N + '</span><i class="fa fa-angle-down"></i><div class="controls"><button class="important">IMPORTANT!</button><button class="delete">DELETE</button></div></li>';
 	
 	return htmlString;
 };
 
 function makeList() { 
 	$.each(items, function (index,value) {
-	console.log( index + ": " + value );
-	/*$.each(items[index],function(key,value) {
-		console.log( key + ": " + value );
-	});*/
+		console.log( index + ": " + value );
 
-	var C = items[index].class,
+		C = items[index].class,
 		I = index,
 		N = items[index].name;
 
-	$('#item-list').append( createItemString( C,I,N ) );
+		$('#item-list').append( createItemString( C,I,N ) );
 	});
 
-	$('.item-count').text(items.length);
+	$('.item-count').text(items.length-1);
 }
 
 
@@ -54,13 +45,10 @@ function makeList() {
 $('#item-form').submit(function(e) {
 	
 	e.preventDefault();
+	$('#item-list').css({"visibility":"visible"});
 
-
-	console.log('Submit!');
-	var newItem = "",
-		newItemString = "";
-
-	newItem = $('input').val();
+	newItem = $('input').val(),
+	newItemString = "";
 
 	if(!newItem) {
 		alert("Oops! You didn't write anything!");
@@ -73,7 +61,7 @@ $('#item-form').submit(function(e) {
 		console.log(items);
 
 		$('#item-list').prepend(newItemString);
-		$('.item-count').text(items.length);
+		$('.item-count').text(items.length-1);
 	}
 
 	this.reset();
@@ -106,18 +94,8 @@ $('#item-list').on('click', '.item-btn', function(e){
 	pos = $(this).parents('.item').attr("value");
 	items.splice(pos, 1);
 	$(this).parents('.item').remove();
-	$('.item-count').text(items.length);
-
-	
+	$('.item-count').text(items.length-1);
 });
-
-// Mark Items as Done
-
-// Mark Items as Not Done
-
-// Mark Itesm as Important
-
-// Delete Items
 
 makeList();
 });
